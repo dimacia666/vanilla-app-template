2693f46dd2f52a2c31a9d35a714b544e536703ee
dimacia666 <dimahotab666@gmail.com>
2025-12-09 16:55:31 +0200

    Make Task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,16 +1,48 @@
 const feedbackForm = document.querySelector(".feedback-form");
-
-
-
+const emailInput = feedbackForm.elements.email;
+const messageInput = feedbackForm.elements.message;
+    
 let formData = {
     email: "",
     message: "",
 };
 
-feedbackForm.addEventListener("input", (event) => {
-    if (event = feedbackForm.email) {
-        formData.email = event.target.value;
-    }
-});
-console.log(formData);
+feedbackForm.addEventListener("input", event => {
+    const elementName = event.target.name;
+    const elementValue = event.target.value;
 
+    formData[elementName] = elementValue;
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+});
+
+const saveValue = localStorage.getItem("feedback-form-state");
+
+if (saveValue) { 
+    const parseValue = JSON.parse(saveValue);
+    formData = parseValue;
+
+    emailInput.value = parseValue.email || "";
+    messageInput.value = parseValue.message || "";
+};
+
+feedbackForm.addEventListener("submit", onSubmit);
+
+function onSubmit(event) { 
+    event.preventDefault();
+
+    if (!formData.email||!formData.message) { 
+        alert("Fill please all fields");
+        return;
+    };
+
+    console.log(formData);
+    
+    formData = {
+        email: "",
+        message: "",
+    }
+
+    localStorage.removeItem("feedback-form-state");
+    feedbackForm.reset();
+};
